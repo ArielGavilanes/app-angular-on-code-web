d9b8900cf591d1991e79427c930f7fa8fdf06065
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-05-15 22:10:21 -0500

    fixing merge

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,23 +3,13 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-
-
-
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenubarModule } from 'primeng/menubar';
 
 @NgModule({
-  declarations: [
-    SidebarComponent,
-
-  ],
-  imports: [
-    CommonModule,
-    SidebarModule,
-    ButtonModule
-  ],
-  exports: [
-    SidebarComponent
-  ]
+  declarations: [SidebarComponent, NavbarComponent, FooterComponent],
+  imports: [CommonModule, SidebarModule, ButtonModule, MenubarModule],
+  exports: [SidebarComponent, NavbarComponent, FooterComponent],
 })
-
-export class SharedModule { }
+export class SharedModule {}
