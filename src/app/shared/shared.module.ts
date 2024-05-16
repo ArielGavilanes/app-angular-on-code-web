813fdc80340b982aafeeb84d456b9d9502046a9c
Danitarapues2 <dpr.tarapues@yavirac.edu.ec>
2024-05-15 21:47:38 -0500

    footer1

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,22 +3,24 @@ import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
-
-
+import { FooterComponent } from './footer/footer.component';
+import { FooterModule } from './footer/footer.module';
 
 
 @NgModule({
   declarations: [
     SidebarComponent,
-    
+    FooterComponent
   ],
   imports: [
     CommonModule,
     SidebarModule,
-    ButtonModule
+    ButtonModule,
+    FooterModule
   ],
   exports: [
-    SidebarComponent
+    SidebarComponent,
+    FooterComponent
   ]
 })
 
