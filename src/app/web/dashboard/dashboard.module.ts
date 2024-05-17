abad5cc8a375c0f728458d813f4a7a8438d0ac36
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-05-17 11:11:50 -0500

    fixing

--- src/app/web/dashboard/dashboard.module.ts
+++ src/app/web/dashboard/dashboard.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 import { DashboardComponent } from './dashboard.component';
 import { CarouselComponent } from './carousel/carousel.component';
-
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    DashboardComponent,
-    CarouselComponent
-  ],
+  declarations: [DashboardComponent, CarouselComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    CarouselModule,
+    ButtonModule,
+    TagModule,
+    SharedModule
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -4,26 +4,18 @@ import { CommonModule } from '@angular/common';
 import { WebRoutingModule } from './web-routing.module';
 import { WebComponent } from './web.component';
 import { SharedModule } from '../shared/shared.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { CarouselComponent } from './dashboard/carousel/carousel.component';
-import { CarouselModule } from 'primeng/carousel';
-import { ButtonModule } from 'primeng/button';
-import { TagModule } from 'primeng/tag';
+import { DashboardModule } from './dashboard/dashboard.module';
+
 
 
 @NgModule({
   declarations: [
     WebComponent,
-    DashboardComponent,
-    CarouselComponent
   ],
   imports: [
     CommonModule,
     WebRoutingModule,
-    SharedModule,
-    CarouselModule,
-    ButtonModule,
-    TagModule
+    DashboardModule
   ]
 })
 export class WebModule { }
