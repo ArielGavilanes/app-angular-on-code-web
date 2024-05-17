58a91f6c03e8491d7887fe9d2aac9f3e46464059
dlcp <dlc.perez@yavirac.edu.ec>
2024-05-17 09:23:49 -0500

    cursos lista

--- src/app/categorias/cursos-por-categoria/cursos-list/cursos-list.component.ts
+++ src/app/categorias/cursos-por-categoria/cursos-list/cursos-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+
 
 @Component({
   selector: 'cursos-list',
@@ -7,4 +8,11 @@ import { Component, Input } from '@angular/core';
 })
 export class CursosListComponent {
   @Input() cursos: any[] = [];
+
+  @Output() verDetallesClick: EventEmitter<number> = new EventEmitter<number>();
+
+  onVerDetallesClick(id: number) {
+    this.verDetallesClick.emit(id);
+  }
+
 }
--- src/app/categorias/cursos-por-categoria/cursos-por-categoria.module.ts
+++ src/app/categorias/cursos-por-categoria/cursos-por-categoria.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosPorCategoriaComponent } from './cursos-por-categoria.component';
 import { CursosListComponent } from './cursos-list/cursos-list.component';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [CursosPorCategoriaComponent, CursosListComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CardModule
+  ],
 })
 export class CursosPorCategoriaModule {}
