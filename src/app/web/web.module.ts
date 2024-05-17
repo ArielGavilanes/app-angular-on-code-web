1857466acdfa319c564a0055f7280da3f9410b21
Danitarapues2 <dpr.tarapues@yavirac.edu.ec>
2024-05-17 01:58:07 -0500

    Carousel1

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -4,16 +4,26 @@ import { CommonModule } from '@angular/common';
 import { WebRoutingModule } from './web-routing.module';
 import { WebComponent } from './web.component';
 import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CarouselComponent } from './dashboard/carousel/carousel.component';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
+import { TagModule } from 'primeng/tag';
 
 
 @NgModule({
   declarations: [
-    WebComponent
+    WebComponent,
+    DashboardComponent,
+    CarouselComponent
   ],
   imports: [
     CommonModule,
     WebRoutingModule,
-    SharedModule
+    SharedModule,
+    CarouselModule,
+    ButtonModule,
+    TagModule
   ]
 })
 export class WebModule { }
